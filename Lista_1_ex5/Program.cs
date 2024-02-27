66bf956646c9e783103e97c9fdbf083f6b1877d5
mentarios <matmentarios@gmail.com>
2024-02-27 15:20:06 -0300

    oi

---
+++ Lista_1_ex5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex5
+{
+    internal class Program
+    {
+        // oi
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("digite o primeiro numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o segundo numero");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o terceiro numero");
+            c = int.Parse(Console.ReadLine());
+            if(a == b  && b == c)
+            {
+                Console.WriteLine("é um triangulo equilatero");
+            }
+            else if( a == b && b != c && ((a + b) < c && (a + c) < b && (b + c) < a))
+            {
+                Console.WriteLine("é um triangulo isósceles");
+            }
+            else if ( a != b && a != c && b != c && ((a+b)<c && (a+c)<b && (b+c)<a))
+            {
+                Console.WriteLine("é um triangulo escaleno");
+            }
+            else { Console.WriteLine("não é um triangulo") ; }
+        }
+    }
+}
